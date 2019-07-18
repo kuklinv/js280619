14024ca49007c79735b9e6d91e83d36851723cc7
kuklinv <vitalykuklin@gmail.com>
2019-07-18 17:50:26 +0700

    continue and near finished lesson4 task1

--- lesson-4/exeL4/exe4_1.js
+++ lesson-4/exeL4/exe4_1.js
@@ -1,12 +1,13 @@
 // task 1
 
-let linksElements = document.querySelectorAll('a');
-for (let i = 0; i < linksElements.length; i += 1) {
-  if (linksElements[i].matches('a[href^=""]')) {
-  	linksElements[i].classList.add('external');
+let linksElements = [...document.querySelectorAll('a')];
+linksElements.forEach(function(elem) {
+  if (elem.matches('a[href^=http]')) {
+    elem.classList.add('external');
   }
-}
+});
 
 
+//  'a[href^=http]'
 
 
