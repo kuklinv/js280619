15a5e69fd079428e9476ea0decae301a2dd4ec2f
kuklinv <vitalykuklin@gmail.com>
2019-07-22 17:19:29 +0700

    for tasks

--- myTask/l4_t2_createObjectTree.js
+++ myTask/l4_t2_createObjectTree.js
@@ -1,53 +1,40 @@
-// let data = {
-//     "Рыбы": {
-//         "Форель": {},
-//         "Щука": {}
-//     },
-//
-//     "Деревья": {
-//         "Хвойные": {
-//             "Лиственница": {},
-//             "Ель": {}
-//         },
-//         "Цветковые": {
-//             "Берёза": {},
-//             "Тополь": {}
-//         }
-//     }
-// };
-//
-// const pageContainer = document.querySelector('div[class="task2"]');
-//
-// const createTree = (htmlStructure, dataTree) => {
-//     let ulElement = document.createElement('ul');
-//     let liElement = document.createElement('li');
-//     let fragment = document.createDocumentFragment();
-//     const iterDataTree = (dataTree) => {
-//         for (let key in dataTree) {
-//             ulElement = key;
-//             if (!dataTree.hasOwnProperty(key)) {
-//
-//             }
-//         }
-//
-//
-//         return
-//     };
-//
-//
-//     for (##
-// )
-//     {
-//         fragment.appendChild();
-//     }
-//     htmlStructure.appendChild(fragment);
-// };
-//
-// function isEmpty(obj) {
-//     for (let key in obj) {
-//         return false;
-//     }
-//     return true;
-// };
-//
-// createTree(pageContainer, data);
+let data = {
+    "Рыбы": {
+        "Форель": {},
+        "Щука": {}
+    },
+
+    "Деревья": {
+        "Хвойные": {
+            "Лиственница": {},
+            "Ель": {}
+        },
+        "Цветковые": {
+            "Берёза": {},
+            "Тополь": {}
+        }
+    }
+};
+
+const pageContainer = document.querySelector('div[class="task2"]');
+let fragment = document.createDocumentFragment();
+const createTree = (htmlStructure, dataTree) => {
+  for (let externalKey in dataTree ) {
+      if (isEmpty(externalKey)) {
+          let  liElem = document.CreateElement('li');
+          liElem.innerHTML = `<span>[externalKey]</span>`
+      }
+  }
+};
+
+// fragment.appendChild();
+// htmlStructure.appendChild(fragment);
+
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+};
+
+createTree(pageContainer, data);
