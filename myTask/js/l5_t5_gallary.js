13e734f6183ebb070b670936638b84f8712918c4
kuklinv <vitalykuklin@gmail.com>
2019-08-06 16:20:06 +0700

    add index2

---
+++ myTask/js/l5_t5_gallary.js
@@ -0,0 +1,13 @@
+let largeImg = document.querySelector('#largeImg');
+let ulTumbs = document.querySelector('#tumbs');
+// console.log(ulTumbs);
+// console.log(largeImg);
+
+ulTumbs.addEventListener('click', function (e) {
+   let target = e.target;
+   if (target.tagName != 'li') {
+       return;
+   }
+
+
+});
