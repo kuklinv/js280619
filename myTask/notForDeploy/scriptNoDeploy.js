6e7ea4b13e86f7de783e2e8cb42b5eb077f3434c
Vitaly Kuklin <vitalykuklin@gmail.com>
2019-08-13 07:49:56 +0700

    add logg decorators and few task

--- myTask/notForDeploy/scriptNoDeploy.js
+++ myTask/notForDeploy/scriptNoDeploy.js
@@ -1,22 +1,81 @@
+// Дорешиваем
+// https://learn.javascript.ru/decorators
+//     https://learn.javascript.ru/prototype
+//         https://learn.javascript.ru/settimeout-setinterval
 
-function f (a) {                          //TODO пока не заботае логгирующий декоратор
-    console.log(a);
+function f(x) {
+    return Math.random() * x; // random для удобства тестирования TODO not work
 }
-let  logArr = [];
-work = makeLogging(f, logArr);
-work(1);
-work(2);
-work(3);
-for (let i = 0; i < logArr.length; i += 1) {
-    console.log(`arg${i} = ${logArr[i]}`);
+function makeCaching(f) {
+    let savedArgs;
+    function wrapper(x) {
+        if (x == savedArgs) {
+            return savedArgs;
+        }
+        savedArgs = x;
+        return f.call(this, x);
+    }
+    return wrapper;
 }
 
-function makeLogging (f, logArr) {
-  return function () {
-      logArr.push(arguments);
-      return f.call(this, arguments);
-  }
-}
+f = makeCaching(f);
+
+var a, b;
+
+a = f(1);
+b = f(1);
+alert(a == b); // true (значение закешировано)
+
+b = f(2);
+alert(a == b); // false, другой аргумент => другое значение
+
+// function work(a, b) {
+//     alert(a + b); // work - произвольная функция
+// }
+//
+// function makeLogging(f, log) {
+//     function wrapper(...args) {
+//     log.push([...args]);
+//     return f.apply(this, args);
+//     }
+//     return wrapper;
+// }
+//
+// var log = [];
+// work = makeLogging(work, log);
+//
+// work(1, 2); // 3
+// work(4, 5); // 9
+//
+// for (var i = 0; i < log.length; i++) {
+//     var args = log[i]; // массив из аргументов i-го вызова
+//     alert('Лог:' + args.join()); // "Лог:1,2", "Лог:4,5"
+// }
+
+// function f(a) {
+//     console.log(a);
+// }
+//
+// let logArr = [];
+// work = makeLogging(f, logArr);
+// work(1);
+// work(2);
+// work(3);
+// console.log(logArr);
+// for (let i = 0; i < logArr.length; i += 1) {
+//     // console.log(`arg${i} = ${logArr[i]}`);
+//     console.log('arg'+ i + '=' + logArr[i]);
+// }
+//
+// function makeLogging(f, logArr) {
+//     return function (a) {
+//         // logArr.push(arguments[0]);
+//         logArr.push(a);
+//         return f.call(this, a);
+//     }
+// }
+//
+
 
 //
 // function f(a) {
@@ -59,11 +118,6 @@ function makeLogging (f, logArr) {
 //
 //     return wrapper;
 // }
-
-
-
-
-
 
 
 // let f = debounce(alert, 1000);
