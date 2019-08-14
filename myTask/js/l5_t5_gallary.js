24c07bc0995e48877dfaf0e8db8db81b1e6fb54d
Vitaly Kuklin <vitalykuklin@gmail.com>
2019-08-14 23:25:45 +0700

    gallary done

--- myTask/js/l5_t5_gallary.js
+++ myTask/js/l5_t5_gallary.js
@@ -1,12 +1,11 @@
 let largeImg = document.querySelector('#largeImg');
 let ulTumbs = document.querySelector('#tumbs');
-// console.log(ulTumbs);
-// console.log(largeImg);
-
 ulTumbs.addEventListener('click', function (e) {
-    e.preventDefault(); //TODO сейчас остановлен вопрос по поводу дефолтного поведенич при нажатии - кгда убираем галлерею. но во первых пропадает галлерея. во вторых выводиться один большой имадж
-   if (target.tagName != 'IMG') {
+    e.preventDefault();
+   let target = e.target;
+    if (target.tagName != 'IMG') {
        return;
    }
-   largeImg.setAttribute('src', 'target.src')
+    let targetHref = target.parentElement.href;
+    largeImg.setAttribute('src', `${targetHref}`);
 });
