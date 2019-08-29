3d01c6e9b4206a2fdc1f204fceffbea0b29088de
Vitaly <vitalykuklin@gmail.com>
2019-08-29 17:03:04 +0700

    clock continue

--- myTask/vidgets/clock/clock.js
+++ myTask/vidgets/clock/clock.js
@@ -6,16 +6,25 @@ class Clock {
         this.minutes = '00';
         this.seconds = '00';
         this.startRender();
-        // this.element.querySelector('.start').addEventListener('click', this.start.bind(this));
-        // this.element.querySelector('.stop').addEventListener('click', this.stop.bind(this));
+        document.querySelector('.start').addEventListener('click', this.start.bind(this));
+        document.querySelector('.stop').addEventListener('click', this.stop.bind(this));
     }
 
     start() {
+        // this.noFormatednow = new Date();
+        // let formatter = new Intl.DateTimeFormat("ru", {
+        //     hour: "numeric",
+        //     minute: "numeric",
+        //     second: "numeric"
+        // });
+        // this.now = formatter.format(this.noFormatednow);
+        // https://learn.javascript.ru/intl
         this.now = new Date();
         this.hours = this.now.getHours();
         this.minutes = this.now.getMinutes();
         this.seconds = this.now.getSeconds();
         this.element.innerHTML = `<output class="hours">${this.hours}</output><output class="minutes">: ${this.minutes}</output><output class="sec">: ${this.seconds}</output>`;
+        setTimeout('start.apply(this, arguments)', 1000); // TODO not work
     }
 
     stop() {
@@ -28,7 +37,35 @@ class Clock {
 }
 
 let clock = new Clock(document.querySelector('.time'));
-console.log(clock);
+// let t = setTimeout('clock.start()', 500);
+// console.log(clock);
 
-// clock.start(); // старт
+clock.start(); // старт
 // clock.stop(); // стоп
+
+// function startTime()
+// {
+//     var tm=new Date();
+//     var h=tm.getHours();
+//     var m=tm.getMinutes();
+//     var s=tm.getSeconds();
+//     m=checkTime(m);
+//     s=checkTime(s);
+//     document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+//     t=setTimeout('startTime()',500);
+// }
+// function checkTime(i)
+// {
+//     if (i<10)
+//     {
+//         i="0" + i;
+//     }
+//     return i;
+// }
+
+// function Timer() {
+//     var dt=new Date()
+//     document.getElementById('time').innerHTML=dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds();
+//     setTimeout("Timer()",1000);
+// }
+// Timer();
