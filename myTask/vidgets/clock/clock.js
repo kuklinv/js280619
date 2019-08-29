9e3b0768140a1f2b22574cd7368034a3a9968f2b
Vitaly <vitalykuklin@gmail.com>
2019-08-29 07:55:01 +0700

    clock

--- myTask/vidgets/clock/clock.js
+++ myTask/vidgets/clock/clock.js
@@ -1,27 +1,34 @@
 class Clock {
     constructor(element) {
         this.element = element;
+        // this.now = new Date();
+        this.hours = '00';
+        this.minutes = '00';
+        this.seconds = '00';
+        this.startRender();
+        // this.element.querySelector('.start').addEventListener('click', this.start.bind(this));
+        // this.element.querySelector('.stop').addEventListener('click', this.stop.bind(this));
+    }
+
+    start() {
         this.now = new Date();
         this.hours = this.now.getHours();
         this.minutes = this.now.getMinutes();
         this.seconds = this.now.getSeconds();
-        // this.outputDiv = document.querySelector('.time');
-        this._render();
-        // this.outputH = document.querySelector('.hours');
-        // this.outputM = document.querySelector('.minutes');
-        // this.outputS = document.querySelector('.sec');
+        this.element.innerHTML = `<output class="hours">${this.hours}</output><output class="minutes">: ${this.minutes}</output><output class="sec">: ${this.seconds}</output>`;
     }
-    start () {
 
+    stop() {
+        this.element.innerHTML = `<output class="hours">${this.hours}</output><output class="minutes">: ${this.minutes}</output><output class="sec">: ${this.seconds}</output>`;
     }
-    stop () {
 
-    }
-    _render () {             //TODO not rendering
-        this.innerHTML = `<output class="hours">${this.hours}</output><output class="minutes">${this.minutes}</output><output class="sec">${this.seconds}</output>`;
+    startRender() {
+        this.element.innerHTML = `<output class="hours">${this.hours}</output><output class="minutes">: ${this.minutes}</output><output class="sec">: ${this.seconds}</output>`;
     }
 }
-let clock = new Clock(document.querySelector('.time'));
 
-clock.start(); // старт
-clock.stop(); // стоп
+let clock = new Clock(document.querySelector('.time'));
+console.log(clock);
+
+// clock.start(); // старт
+// clock.stop(); // стоп
