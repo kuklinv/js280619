af607dc463f884e7c8333106e8458365c5d31949
Vitaly <vitalykuklin@gmail.com>
2019-08-30 12:19:51 +0700

    wtf clock

--- myTask/vidgets/clock/clock.js
+++ myTask/vidgets/clock/clock.js
@@ -2,7 +2,7 @@ class Clock {
     constructor(element) {
         this.element = element;
         this.render();
-        document.querySelector('.start').addEventListener('click', this.start());
+        let timer;
     }
 
     formatTime(item) {
@@ -21,50 +21,16 @@ class Clock {
         this.element.innerHTML = `<output class="hours">${h}</output><output class="minutes">: ${min}</output><output class="sec">: ${sec}</output>`;
     }
 
+    stop() {
+        clearInterval(timer);
+    }
+
     start() {
-        setInterval('this.render()', 1000);
+        // debugger
+        timer = setInterval('this.render', 1000);
     }
 }
 
 let clock = new Clock(document.querySelector('.time'));
+document.querySelector('.start').addEventListener('click', clock.start());  // TODO not work
 
-// class Clock {
-//     constructor(element) {
-//         this.element = element;
-//         this.hours = '00';
-//         this.minutes = '00';
-//         this.seconds = '00';
-//         this.startRender();
-//         document.querySelector('.start').addEventListener('click', this.start.bind(this));
-//         document.querySelector('.stop').addEventListener('click', this.stop.bind(this));
-//     }
-//
-//     start() {
-//         this.now = new Date();
-//         this.hours = this.now.getHours();
-//         this.minutes = this.now.getMinutes();
-//         this.seconds = this.now.getSeconds();
-//         let h = this.formatTime(this.hours);
-//         let min = this.formatTime(this.minutes);
-//         let sec = this.formatTime(this.seconds);
-//         this.element.innerHTML = `<output class="hours">${h}</output><output class="minutes">: ${min}</output><output class="sec">: ${sec}</output>`;
-//         setInterval('this.start', 1000); // TODO not work
-//     }
-//
-//     stop() {
-//
-//     }
-//
-//     startRender() {
-//         this.element.innerHTML = `<output class="hours">${this.hours}</output><output class="minutes">: ${this.minutes}</output><output class="sec">: ${this.seconds}</output>`;
-//     }
-//     formatTime(item) {
-//         if (item < 10) return "0" + item;
-//         else return item;
-//     }
-// }
-//
-// let clock = new Clock(document.querySelector('.time'));
-//
-// // clock.start(); // старт
-// // clock.stop(); // стоп
