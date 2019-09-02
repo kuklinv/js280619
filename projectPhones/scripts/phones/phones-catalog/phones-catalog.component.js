2ebe2f84be63c944a1c75e4e6d0eb13b0a4eb7d8
Vitaly <vitalykuklin@gmail.com>
2019-09-02 08:09:21 +0700

    Feature: add phones details and base [ref: fix-not render]

--- projectPhones/scripts/phones/phones-catalog/phones-catalog.component.js
+++ projectPhones/scripts/phones/phones-catalog/phones-catalog.component.js
@@ -1,15 +1,27 @@
+import {BaseComponent} from "../common/base/base.component.js";
 
-export class PhonesCatalogComponent {
-    constructor ({element, phones}) {
-        this._element = element;
+export class PhonesCatalogComponent extends BaseComponent {
+    constructor({element, phones, onPhoneSelect}) {
+        // this._element = element;
+        super({element});
         this._phones = phones;
+        this._onPhoneSelect = onPhoneSelect;
         this._render();
+        this._element.addEventListener('click', (e) => {
+            let phoneEl = e.target.closest('.phone');
+            if (!phoneEl) {
+                return;
+            }
+            // console.log(phoneEl.dataset.phoneId);
+            this._onPhoneSelect(phoneEl.dataset.phoneId);
+        })
     }
+
     _render() {
         this._element.innerHTML = `
         <ul class="phones">
         ${this._phones.map((phone) => `
-        <li class="thumbnail">
+        <li class="thumbnail phone" data-phone-id = ${phone.id}>
             <a href="#!/phones/${phone.id}" class="thumb">
               <img alt="${phone.name}" src="${phone.imageUrl}">
             </a>
--- projectPhones/scripts/phones/phones.component.js
+++ projectPhones/scripts/phones/phones.component.js
@@ -1,14 +1,25 @@
 import {PhonesCatalogComponent} from "./phones-catalog/phones-catalog.component.js";
 import {PhonesService} from "./phones.service.js";
+import {PhoneDetailsComponent} from "./phone-details/phone-details.component.js";
+
 export class PhonesComponent {
     constructor({element}) {
         this._element = element;
         this._render();
         this._catalog = new PhonesCatalogComponent({
             element: this._element.querySelector('.phones-catalog'),
-            phones: PhonesService.getAll()
-        })
+            phones: PhonesService.getAll(),
+            onPhoneSelect: (phoneId) => {
+                const PhoneDetails = PhonesService.getOneById(phoneId);
+                this._catalog.hide();
+                this._phoneDetails.show(PhoneDetails);
+            }
+        });
+        this._phoneDetails = new PhoneDetailsComponent({
+            element: this._element.querySelector('.phone-detail')
+        });
     }
+
 
     _render() {
         this._element.innerHTML = `
@@ -43,6 +54,7 @@ export class PhonesComponent {
 
       <!--Main content-->
       <div class="col-md-10">
+        <div class="phone-detail"></div>
         <div class="phones-catalog"></div>
       </div>
     </div>
